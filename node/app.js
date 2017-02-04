cdc0899b1589f8c7535fedce58b16d1ebac34ac0
Patrick <patrick.saegesser@byerley.ch>
2017-02-04 11:22:14 +0100

    backup

--- node/app.js
+++ node/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -21,20 +22,67 @@ const client = new ttn.Client(region, appId, accessKey);
 
 // tools
 
-function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
-}
+function convert_readings(data){
+  // input must be array
+  console.log("*** converting ***");
+  console.log(data);
+  if (data){
+    max_len = data.length;
+    var readings = [];
+    for (var i = 0; i < max_len; i += 2){
+      var double = data.slice(i, i+2);
+      var reading_value = double[0] * 256 + double[1];
+      readings.push(reading_value);
+    }
+  
+    var output = {};
+    output.bat = readings[0];
+    output.tmp = readings[1] / 100;
+    output.hum = readings[2] / 100;
+    output.ts = new Date();
+  
+    return output;
 
-function str2ab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i=0, strLen=str.length; i<strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
+  } else {
+    console.log("no data. input is " + data);
   }
-  return buf;
 }
 
 
+// DB setup
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection url
+var url = 'mongodb://localhost:4321/ttn'; // mongodb://localhost:4321/ttn
+
+function readingMongoInsert(ttnMsg, sensordata) {
+
+  MongoClient.connect(url, function(err, db) {
+    // Use the admin database for the operation
+    assert.equal(null, err);
+
+    console.log("db connected");
+    var collection = db.collection('makezh');
+    var reading = {};
+    
+    reading.raw = ttnMsg; // saving complete message
+
+    reading.ts = sensordata.ts;
+    reading.bat = sensordata.bat;
+    reading.tmp = sensordata.tmp;
+    reading.hum = sensordata.hum;
+
+    collection.insert(reading, function(err, result){
+      console.log(result);
+    });
+    
+    // List all the available databases
+    db.close();
+
+  });
+
+}
 // run  it
 client.on('connect', function(connack) {
   console.log('[DEBUG]', 'Connect:', connack);
@@ -52,54 +100,67 @@ client.on('device', null, 'down/scheduled', function(deviceId, data) {
   console.log('[INFO] ', 'Scheduled:', deviceId, JSON.stringify(data, null, 2));
 });
 
-client.on('message', function(deviceId, data) {
-  console.info('[INFO] ', 'Message:', deviceId, JSON.stringify(data, null, 2));
+client.on('message', function(deviceId, msg) {
 
-  if (data.payload_raw){
-    var data_point = data.payload_raw;
-    //const point = Buffer.from(data_point);
-    console.log("data_point is: " + data_point);
-  }else { 
-    console.log("no payload");
-  }
-
+    var data = new Uint16Array(msg.payload_raw);
+      // check if message contains payload
+      if (data){
+        var sensordata = convert_readings(data); // convert sensor readings in payload_raw
+        readingMongoInsert(msg, sensordata); // insert raw data and converted data to db     
+      // complain if no payload
+      }else { 
+        console.log("no payload");
+      }
 });
 
-// express basic server
+//// express server
 // set up
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routing
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('connection')
-  
-  client.on('message', function(deviceId, data) {
 
-      if (data.payload_raw){
-        
-        var data_point = ab2str(data.payload_raw);
-        io.emit('chat message', data_point);
-
-      }else { 
-        
-        console.log("no payload");
-
-      }
-
-    });
-
-
-  socket.on('chat message', function(msg){
-    // console.log('message: ' + msg);
-    // io.emit('chat message', msg);
-  });
+app.get('/historic', function(req, res){
+  res.sendFile(__dirname + '/historic.html');
 });
 
-//serve
+app.post('/historic',function(req,res){
+   console.log(req.body);
+});
+
+
+// open websocket connection
+io.on('connection', function(socket){
+  
+  console.log('socket connection');
+  // listen for incoming ttn messages;
+  client.on('message', function(deviceId, msg) {
+      console.log("**** MESSAGE RECEIVED ****");
+      console.log(msg);
+      var data = new Uint16Array(msg.payload_raw);
+
+      // check if message contains payload
+      if (data){
+        console.log("we have payload: " + data);
+        var sensordata = convert_readings(data); // convert sensor readings in payload_raw 
+        io.emit('data_refresh', sensordata); // emit socket message to all connected clients
+      
+      // complain if no payload
+      }else { 
+        console.log("no payload");
+      }
+    });
+});
+
+
+// run server
 http.listen(PORT, function(){
   console.log('listening on port*: ' + PORT);
 });
